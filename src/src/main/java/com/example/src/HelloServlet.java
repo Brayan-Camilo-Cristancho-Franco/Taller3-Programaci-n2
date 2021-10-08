aecb348efd0270ce2522ae61d908031931082883
DavidMillan45 <dmillan@unboque.edu.co>
2021-10-08 12:36:36 -0500

    Reading csv

--- src/src/main/java/com/example/src/HelloServlet.java
+++ src/src/main/java/com/example/src/HelloServlet.java
@@ -1,8 +1,14 @@
 package com.example.src;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Optional;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -12,15 +18,42 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
 
-        // Hello
+        Optional<List<Users>> mail = AnaliseCsv.getListOfUsers();
+
+        String templateName;
+
+        if (mail.isPresent()) {
+
+            request.setAttribute("mail", mail.get());
+          //  templateName = "listmails.jsp";
+        } else {
+
+           // templateName = "showError.jsp";
+        }
+
+       // var dispatcher = request.getRequestDispatcher(templateName);
+        //dispatcher.forward(request, response);
+
+        String Username = request.getParameter("username");
+        String Pasword = request.getParameter("pasSword");
+        System.out.println("username "+Username);
+        System.out.println("password "+Pasword);
+        System.out.println(mail+ "lista");
+
+
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>"+"hello" +"</h1>");
         out.println("</body></html>");
     }
+
+
+
+
+
 
     public void destroy() {
     }
